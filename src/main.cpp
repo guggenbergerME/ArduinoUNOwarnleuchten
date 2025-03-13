81a6b713816f7ff9486a633f9b721434eb21adc4
guggenbergerME <github@guggenberger.me>
2025-03-13 14:08:37 +0100

    init

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,13 @@
 #include <SPI.h> // Seriell
 #include <Wire.h>
 
+/************************************************************************** Warnleuchten belegung
+Relais    Farbe     Funktion
+K0        grün      1/0
+K1        gelb      1/0
+K2        rot       1/0
+*/
+
 /************************************************************************** Kabelcode
 gn        SDA
 gn/ 1sw   SDC
@@ -34,6 +41,7 @@ rt        +5V
 */
 
 /************************************************************************** Pinbelegung Arduino UNO
+Analog Eingänge
 A4      SDA -> gn
 A5      SCL -> gn/1sw
 */
@@ -42,7 +50,7 @@ A5      SCL -> gn/1sw
 PCF8574 pcf8574(0x20);
 
 //************************************************************************** LAN Network definieren 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x01 };
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x08 };
 IPAddress ip(10, 110, 0, 5); //comment this line if you are using DHCP
 
 //IPAddress subnet(255, 255, 0, 0); // Subnet Mask
@@ -55,9 +63,6 @@ PubSubClient client(ethClient);
 //************************************************************************** Variablen
 int topic_init = 0; // Topic Variable zum einmaligen aufrufen
 
-int status_tor_auf = 0;
-int status_tor_zu = 0;
-int status_tor_toggle = 0;
 
 
 //************************************************************************** Funktionsprototypen
@@ -83,16 +88,6 @@ unsigned long interval_BEISPIEL = 800;
 unsigned long previousMillis_mqtt_reconnect = 0; // 
 unsigned long interval_mqtt_reconnect = 200; 
 
-// Relais Abfallverzögerung
-unsigned long previousMillis_Tor_Auf;
-unsigned long previousMillis_Tor_Zu;
-unsigned long previousMillis_Tor_toggle; // Schalten des Stromstoss Relais für Tag Öffnung
-
-// Wie lange sollen die Relais halten
-unsigned long dauer_relais_Tor_auf = 1000;
-unsigned long dauer_relais_Tor_zu = 1000;
-unsigned long dauer_relais_Tor_toggle = 1000;
-
 //************************************************************************** SETUP
 void setup() {
   Serial.begin(115200);
@@ -117,15 +112,15 @@ void setup() {
   pcf8574.pinMode(P7, OUTPUT);        
   pcf8574.begin();
 
-/////////////////////////////////////////////////////////////////////////// Portexpander Relais definieren
-pcf8574.digitalWrite(P0, !LOW);
-pcf8574.digitalWrite(P1, !LOW);
-pcf8574.digitalWrite(P2, !LOW);
-pcf8574.digitalWrite(P3, !LOW);
-pcf8574.digitalWrite(P4, !LOW);
-pcf8574.digitalWrite(P5, !LOW);
-pcf8574.digitalWrite(P6, !LOW);
-pcf8574.digitalWrite(P7, !LOW);
+  /////////////////////////////////////////////////////////////////////////// Portexpander Relais definieren
+  pcf8574.digitalWrite(P0, !LOW);
+  pcf8574.digitalWrite(P1, !LOW);
+  pcf8574.digitalWrite(P2, !LOW);
+  pcf8574.digitalWrite(P3, !LOW);
+  pcf8574.digitalWrite(P4, !LOW);
+  pcf8574.digitalWrite(P5, !LOW);
+  pcf8574.digitalWrite(P6, !LOW);
+  pcf8574.digitalWrite(P7, !LOW);
 
 }
 
@@ -140,14 +135,16 @@ Benötigte Variable
   {
     topic_init = 1;
     // Topic init
-    client.publish("Werktor/K0", "online");
-    client.publish("Werktor/K1", "online");
-    client.publish("Werktor/K2", "online");
-    //client.publish("Werktor/K3", "online");
-    //client.publish("Werktor/K4", "online");
-    //client.publish("Werktor/K5", "online");
-    //client.publish("Werktor/K6", "online");
-    //client.publish("Werktor/K7", "online");
+    client.publish("Meldungen/Warnleuchte001/gruen", "online");
+    client.publish("Meldungen/Warnleuchte001/gelb", "online");    
+    client.publish("Meldungen/Warnleuchte001/rot", "online");
+    /*
+    client.publish("Meldungen/Warnleuchte001/K3", "online");    
+    client.publish("Meldungen/Warnleuchte001/K4", "online");    
+    client.publish("Meldungen/Warnleuchte001/K5", "online");
+    client.publish("Meldungen/Warnleuchte001/K6", "online");
+    client.publish("Meldungen/Warnleuchte001/K7", "online");
+    */
   }
 }
 
@@ -155,16 +152,18 @@ Benötigte Variable
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Verbindung zum MQTT-Server aufbauen...");
-    if (client.connect("WerktorClient", "hitesh", "RO9UZ7wANCXzmy")) {
+    if (client.connect("Warnleuchte001", "hitesh", "RO9UZ7wANCXzmy")) {
       Serial.println("verbunden");
-      client.subscribe("Werktor/K0");
-      client.subscribe("Werktor/K1");
-      client.subscribe("Werktor/K2");
-      //client.subscribe("Werktor/K3");
-      //client.subscribe("Werktor/K4");
-      //client.subscribe("Werktor/K5");
-      //client.subscribe("Werktor/K6");
-      //client.subscribe("Werktor/K7");
+      client.subscribe("Meldungen/Warnleuchte001/gruen");
+      client.subscribe("Meldungen/Warnleuchte001/gelb");
+      client.subscribe("Meldungen/Warnleuchte001/rot");
+      /*
+      client.subscribe("Meldungen/Warnleuchte001/K3");
+      client.subscribe("Meldungen/Warnleuchte001/K4");
+      client.subscribe("Meldungen/Warnleuchte001/K5");
+      client.subscribe("Meldungen/Warnleuchte001/K6");
+      client.subscribe("Meldungen/Warnleuchte001/K7");                                    
+      */
     } else {
       Serial.print("Fehler, rc=");
       Serial.print(client.state());
@@ -189,115 +188,109 @@ void callback(char* topic, byte* payload, unsigned int length) {
   
   Serial.println(message);
 
-  // -------------------------------------------------------- Topic Auswerten K0
-  if (String(topic) == "Werktor/K0") {
-    if (message == "auf") {
-        Serial.println("Relais K0 -> Tor AUF Impuls");
-        pcf8574.digitalWrite(P0, !HIGH);
-        status_tor_auf = 1;
-        // Bei aktivierung millis speichern
-        previousMillis_Tor_Auf = millis();
-    } 
-    else if (message == "zu") {
-        Serial.println("Relais K0 -> Tor AUF Relais abschalten");
-        pcf8574.digitalWrite(P0, !LOW);
-        status_tor_auf = 0;
-    } 
-    else { }} else { }
+// -------------------------------------------------------- Topic Auswerten K0
+    if (String(topic) == "Meldungen/Warnleuchte001/gruen") {
+      if (message == "on") {
+          Serial.println("Relais K0 -> AN");
+          pcf8574.digitalWrite(P0, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K0 -> AUS");
+          pcf8574.digitalWrite(P0, !LOW);
+      } 
+      else { }} else { }
 
 // -------------------------------------------------------- Topic Auswerten K1
-  if (String(topic) == "Werktor/K1") {
-    if (message == "zu") {
-        Serial.println("Relais K1 -> Tor ZU Impuls");
-        pcf8574.digitalWrite(P1, !HIGH);
-        status_tor_zu = 1;
-        // Bei aktivierung millis speichern
-        previousMillis_Tor_Zu = millis();        
-    } 
-    else if (message == "zu") {
-        //Serial.println("Relais K1 -> Tor ZU Relais abschalten");
-        //pcf8574.digitalWrite(P1, !LOW);
-    } 
-    else { }} else { }
+    if (String(topic) == "Meldungen/Warnleuchte001/gelb") {
+      if (message == "on") {
+          Serial.println("Relais K1 -> AN");
+          pcf8574.digitalWrite(P1, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K1-> AUS");
+          pcf8574.digitalWrite(P1, !LOW);
+      } 
+      else { }} else { }
 
 // -------------------------------------------------------- Topic Auswerten K2
-  if (String(topic) == "Werktor/K2") {
-    if (message == "toggleAUF") {
-        Serial.println("Toogle Impuls Stromstossrelais");
-        pcf8574.digitalWrite(P2, !HIGH);
-        status_tor_toggle = 1;
-        // Bei aktivierung millis speichern
-        previousMillis_Tor_toggle = millis();           
-    } 
-    else if (message == "toggleZU") {
-        Serial.println("Toogle Impuls Stromstossrelais");
-        pcf8574.digitalWrite(P2, !LOW);
-    } 
-    else { }} else { }
+    if (String(topic) == "Meldungen/Warnleuchte001/rot") {
+      if (message == "on") {
+          Serial.println("Relais K2 -> AN");
+          pcf8574.digitalWrite(P2, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K2-> AUS");
+          pcf8574.digitalWrite(P2, !LOW);
+      } 
+      else { }} else { }
+
 /*
 // -------------------------------------------------------- Topic Auswerten K3
-  if (String(topic) == "Werktor/K3") {
-    if (message == "on") {
-        Serial.println("Relais K3 -> AN");
-        pcf8574.digitalWrite(P3, !HIGH);
-    } 
-    else if (message == "off") {
-        Serial.println("Relais K3 -> AUS");
-        pcf8574.digitalWrite(P3, !LOW);
-    } 
-    else { }} else { }
+    if (String(topic) == "Meldungen/Warnleuchte001/K3") {
+      if (message == "on") {
+          Serial.println("Relais K3 -> AN");
+          pcf8574.digitalWrite(P3, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K3-> AUS");
+          pcf8574.digitalWrite(P3, !LOW);
+      } 
+      else { }} else { }
 
 // -------------------------------------------------------- Topic Auswerten K4
-  if (String(topic) == "Werktor/K4") {
-    if (message == "on") {
-        Serial.println("Relais K4 -> AN");
-        pcf8574.digitalWrite(P4, !HIGH);
-    } 
-    else if (message == "off") {
-        Serial.println("Relais K4 -> AUS");
-        pcf8574.digitalWrite(P4, !LOW);
-    } 
-    else {  }} else {  }
-
+    if (String(topic) == "Meldungen/Warnleuchte001/K4") {
+      if (message == "on") {
+          Serial.println("Relais K4 -> AN");
+          pcf8574.digitalWrite(P4, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K4-> AUS");
+          pcf8574.digitalWrite(P4, !LOW);
+      } 
+      else { }} else { }
 
 // -------------------------------------------------------- Topic Auswerten K5
-  if (String(topic) == "Werktor/K5") {
-    if (message == "on") {
-        Serial.println("Relais K5 -> AN");
-        pcf8574.digitalWrite(P5, !HIGH);
-    } 
-    else if (message == "off") {
-        Serial.println("Relais K5 -> AUS");
-        pcf8574.digitalWrite(P5, !LOW);
-    } 
-    else { }} else { }
+    if (String(topic) == "Meldungen/Warnleuchte001/K5") {
+      if (message == "on") {
+          Serial.println("Relais K5 -> AN");
+          pcf8574.digitalWrite(P5, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K5-> AUS");
+          pcf8574.digitalWrite(P5, !LOW);
+      } 
+      else { }} else { }
 
 // -------------------------------------------------------- Topic Auswerten K6
-  if (String(topic) == "Werktor/K6") {
-    if (message == "on") {
-        Serial.println("Relais K6 -> AN");
-        pcf8574.digitalWrite(P6, !HIGH);
-    } 
-    else if (message == "off") {
-        Serial.println("Relais K6 -> AUS");
-        pcf8574.digitalWrite(P6, !LOW);
-    } 
-    else { }} else { }
+    if (String(topic) == "Meldungen/Warnleuchte001/K6") {
+      if (message == "on") {
+          Serial.println("Relais K6 -> AN");
+          pcf8574.digitalWrite(P6, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K6-> AUS");
+          pcf8574.digitalWrite(P6, !LOW);
+      } 
+      else { }} else { }
 
 // -------------------------------------------------------- Topic Auswerten K7
-  if (String(topic) == "Werktor/K7") {
-    if (message == "on") {
-        Serial.println("Relais K7 -> AN");
-        pcf8574.digitalWrite(P7, !HIGH);
-    } 
-    else if (message == "off") {
-        Serial.println("Relais K7 -> AUS");
-        pcf8574.digitalWrite(P7, !LOW);
-    } 
-    else { }} else { }
+    if (String(topic) == "Meldungen/Warnleuchte001/K7") {
+      if (message == "on") {
+          Serial.println("Relais K7 -> AN");
+          pcf8574.digitalWrite(P7, !HIGH);
+      } 
+      else if (message == "off") {
+          Serial.println("Relais K7-> AUS");
+          pcf8574.digitalWrite(P7, !LOW);
+      } 
+      else { }} else { }      
+
 */
 
 }
+
+
+
 //************************************************************************** mqtt_reconnect_intervall 
 void mqtt_reconnect_intervall() {
     if (!client.connected()) {
@@ -309,50 +302,20 @@ void mqtt_reconnect_intervall() {
 //************************************************************************** LOOP
 void loop() {
 
- //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Arduino Reset
-//if ( millis()  >= 780000) resetFunc(); // Reset alle 13 Min
-if ( millis()  >= 60000) resetFunc(); // Reset alle 13 Min
-
 // MQTT Abfrage
     if (millis() - previousMillis_mqtt_reconnect > interval_mqtt_reconnect) {
       previousMillis_mqtt_reconnect = millis(); 
       mqtt_reconnect_intervall();
     }  
 
-// Relais Ausschaltverzögerung - Tor AUF
-if (millis() - previousMillis_Tor_Auf > dauer_relais_Tor_auf && status_tor_auf == 1)  
-  {
-        pcf8574.digitalWrite(P0, !LOW);
-        Serial.println("P0/K0 - AUS");
-        status_tor_auf = 0;
-  } 
-// Relais Ausschaltverzögerung - Tor ZU
-if (millis() - previousMillis_Tor_Zu > dauer_relais_Tor_zu && status_tor_zu == 1)  
-  {
-        pcf8574.digitalWrite(P1, !LOW);
-        Serial.println("P1/K1 - AUS");
-        status_tor_zu = 0;
-  }   
-
-// Relais Toggle - Schaltung Pförtner für dauer auf
-if (millis() - previousMillis_Tor_toggle > dauer_relais_Tor_toggle && status_tor_toggle == 1)  
-  {
-        pcf8574.digitalWrite(P2, !LOW);
-        Serial.println("P2/K2 - AUS");
-        status_tor_toggle = 0;
-  }  
-
-
-
-/*
-        delay(1000);
-        pcf8574.digitalWrite(P0, !LOW);
-        Serial.println("P0 - AUS");
-        status_tor_auf = 0;
-  */    
-
     
 // Mqtt Topic einmalig beim Start INIT
 topic_mqtt_init();
+
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Arduino Reset
+//if ( millis()  >= 780000) resetFunc(); // Reset alle 13 Min
+if ( millis()  >= 780000) resetFunc(); // Reset alle 13 Min
+
+
 
 }
